9b84a1dbf749667d1cb831aedd5a85fdef8ef0f4
manjunathnp <mail.manjunathnp@gmail.com>
2024-03-14 14:28:53 +0530

    updated codes

--- src/test/java/com/essentials/HandleWindows.java
+++ src/test/java/com/essentials/HandleWindows.java
@@ -1,7 +1,5 @@
 package com.essentials;
 
-import com.base.TestBase;
-import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
